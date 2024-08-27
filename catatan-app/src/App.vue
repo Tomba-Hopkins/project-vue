952681b0191f48ff484e9662b200a89971d47ce1
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-08-27 11:36:34 +0700

    belajar vite

--- catatan-app/src/App.vue
+++ catatan-app/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    <h1 class="text-white">The Notes Memories</h1>
+    <h1 class="bg-gradient-to-tl from-sky-400 to-slate-600">The Notes Memories</h1>
     <p>Help you for remember any words idea or anything else, so enjoy it🦖</p>
     <main>
-      <h2 class="text-red-500">List of memo</h2>
-      <div class="card"></div>
+      <h2 class="text-red-800">List of memo</h2>
+      <div class="card bg-slate-800"></div>
+      <p class="text-sky-400">HUAH</p>
     </main>
   </div>
 </template>
+
+
