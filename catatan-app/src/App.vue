cda27a24a65ea16352f4ff7ce0ee1d2006e8e51c
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-08-27 11:03:47 +0700

    saat ku jumpa dirinya

--- catatan-app/src/App.vue
+++ catatan-app/src/App.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1 class="text-slate-500 text-center mt-5 text-3xl font-mono">Hello</h1>
+  <div>
+    <h1 class="text-white">The Notes Memories</h1>
+    <p>Help you for remember any words idea or anything else, so enjoy it🦖</p>
+    <main>
+      <h2 class="text-red-500">List of memo</h2>
+      <div class="card"></div>
+    </main>
+  </div>
 </template>
