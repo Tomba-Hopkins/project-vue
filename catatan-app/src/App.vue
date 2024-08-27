1634539441af40dd9d0d9969959592bb5cae9bf8
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-08-27 13:27:33 +0700

    sidii to sidiii

--- catatan-app/src/App.vue
+++ catatan-app/src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <h1 class="bg-gradient-to-tl from-sky-400 to-slate-600">The Notes Memories</h1>
-    <p>Help you for remember any words idea or anything else, so enjoy it🦖</p>
+  <div class="p-2">
+    <h1 class="bg-gradient-to-bl from-sky-400 to-slate-600 p-2 text-5xl text-center text-transparent bg-clip-text font-semibold shadow-xl mt-3">The Notes Memories</h1>
+    <p class="text-center bg-gradient-to-r from-sky-700 to-slate-200 bg-clip-text text-transparent">Help you for remember any words idea or anything else, so enjoy it <span class="text-white">🦖</span></p>
     <main>
-      <h2 class="text-red-800">List of memo</h2>
-      <div class="card bg-slate-800"></div>
-      <p class="text-sky-400">HUAH</p>
+      <h2 class="text-4xl text-center font-bold bg-gradient-to-r from-slate-700 to-slate-100 bg-clip-text text-transparent mt-6">List of memo</h2>
+      <hr class="border-1 mt-6 border-sky-600">
+      <button class="p-2 w-1/12 mt-14 bg-sky-300 text-slate-600 font-semibold font-mono rounded-md absolute right-10 hover:bg-sky-600 hover:text-slate-100 transition duration-150 active:bg-sky-800 active:text-slate-100">+</button>
     </main>
   </div>
 </template>
