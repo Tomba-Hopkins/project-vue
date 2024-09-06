b0227f27cbd92aa8b16b3e9d83f4ad9e798d6868
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-09-06 17:12:03 +0700

    router

--- menu-warung/src/views/Menu.vue
+++ menu-warung/src/views/Menu.vue
@@ -1,12 +1,14 @@
 <script setup>
 
-import { useRoute, RouterView } from 'vue-router';
+import { useRoute, RouterView, useRouter } from 'vue-router';
 import menus from '../data/menu.json'
 import { computed } from 'vue';
 
 const route = useRoute()
+const router = useRouter()
 
 const type = route.params.type
+const dataID = parseInt(route.params.id)
 
 
 const data = computed(() => {
@@ -14,12 +16,22 @@ const data = computed(() => {
     const tipe = menus.find((menu) => menu.title.toLowerCase() === type.toLowerCase())
 
     if (tipe) {
-        return tipe.items.find((item) => item.id === parseInt(route.params.id))
+        return tipe.items.find((item) => item.id === dataID)
     } else {
         return null
     }
     
 })
+
+
+const showBonus = () => {
+    router.push({
+        name: "bonus",
+        params: {
+            id: dataID
+        }
+    })
+}
 
 </script>
 
@@ -30,7 +42,7 @@ const data = computed(() => {
             <h2>{{ data.name }}</h2>
             <img :src="data.img" :alt="data.name">
             <p>Price: {{ data.price }}</p>
-            <button @click="bonButton = !bonButton">Bonus</button>
+            <button @click="showBonus()">Bonus</button>
             <RouterView/>
         </div>
         <h1 v-else>Not found</h1>
