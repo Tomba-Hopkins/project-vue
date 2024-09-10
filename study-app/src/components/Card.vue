061ac0943b18ef08020352ab93140bd5544157a7
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-09-10 09:05:39 +0700

    shshshshsh

--- study-app/src/components/Card.vue
+++ study-app/src/components/Card.vue
@@ -1,7 +1,28 @@
+<script setup>
+
+import mapels from '../data/soal.json'
+
+
+</script>
+
+
 <template>
-    <div class="card">
-            <img src="" alt="">
-            <h2></h2>
-            <p>s</p>
+    <main>
+        <div class="card" v-for="mapel in mapels" :key="mapel.id">
+            <img :src="mapel.img" :alt="mapel.name">
+            <h2>{{ mapel.name }}</h2>
+            <p>{{ mapel.soal.length }}</p>
         </div>
+    </main>
 </template>
+
+
+<style scoped>
+    .main {
+        width: 100%;
+        padding: 1rem;
+        margin-top: 2rem;
+        display: flex;
+        flex-wrap: wrap;
+    }
+</style>
