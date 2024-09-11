4b5323ba2ef8f18597308c42a227059fc0b3f3a6
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-09-11 09:44:51 +0700

    fuf

--- study-app/src/components/Card.vue
+++ study-app/src/components/Card.vue
@@ -1,28 +1,86 @@
 <script setup>
 
 import mapels from '../data/soal.json'
+import gsap from 'gsap';
+
+
+const bEnter = (el) => {
+    el.style.opacity = '0'
+    el.style.transform = 'translateX(-5rem)'
+}
+const enter = (el) => {
+    gsap.to(el, {
+        x: '0',
+        opacity: '1',
+        duration: 0.3,
+        delay: el.dataset.dek * 0.3
+    })
+}
 
 
 </script>
 
 
 <template>
-    <main>
-        <div class="card" v-for="mapel in mapels" :key="mapel.id">
-            <img :src="mapel.img" :alt="mapel.name">
-            <h2>{{ mapel.name }}</h2>
-            <p>{{ mapel.soal.length }}</p>
-        </div>
-    </main>
+    <section>
+        <TransitionGroup appear @before-enter="bEnter" @enter="enter">
+            <div class="card" v-for="(mapel, index) in mapels" :key="mapel.id" :data-dek="index">
+                <img :src="mapel.img" :alt="mapel.name">
+                <div class="content">
+                    <h2>{{ mapel.name }}</h2>
+                    <p>{{ mapel.soal.length }} Soal</p>
+                </div>
+            </div>
+        </TransitionGroup>
+    </section>
 </template>
 
 
 <style scoped>
-    .main {
+    section {
         width: 100%;
         padding: 1rem;
-        margin-top: 2rem;
+        margin-top: 5rem;
         display: flex;
         flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+        gap: 3rem;
+    }
+
+    .card {
+        width: 12rem;
+        height: 14rem;
+        background-color: #ccc;
+        border-radius: 1rem;
+        color: black;
+        display: flex;
+        justify-content: space-between;
+        flex-direction: column;
+        transition: all 0.4s ease;
+        cursor: pointer;
+    }
+
+    .card:hover {
+        width: 14rem;
+    }
+
+    .card img {
+        width: 100%;
+        height: 50%;
+        border-radius: 1rem 1rem 0 0;
+        padding: -5rem;
+        filter: grayscale(0);
+        transition: all 0.4s ease;
+    }
+
+    .card .content {
+        width: 100%;
+        height: 50%;
+        padding: 1rem;
+    }
+
+    .card img:hover {
+        filter: grayscale(1);
     }
 </style>
--- study-app/src/view/Dashboard.vue
+++ study-app/src/view/Dashboard.vue
@@ -9,8 +9,32 @@ import Card from '@/components/Card.vue';
     <main>
         <header>
             <h1>Welcome</h1>
-            <input type="search">
+            <input type="search" autofocus>
         </header>
         <Card/>
     </main>
 </template>
+
+
+<style scoped>
+
+main {
+    max-width: 90vw;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+    flex-direction: column;
+}
+
+header {
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+}
+
+</style>
